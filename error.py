169008f9a2a93eb033d46c5844c636e1a7c08b64
814725243014-sketch <814725243014@trp.srmtrichy.edu.in>
2026-02-16 14:48:20 +0530

    Update and rename program.py to error.py

---
+++ error.py
@@ -0,0 +1,9 @@
+ try:
+   a=int(input("Enter a Num:")
+   b=int(input("Enter another number:")
+   result=a/b
+   print("Result:",result)
+ except zero division error:
+   print("Error:,Division by zero is not allowed")
+ except value error:
+   print("Error:please enter a valid integer")
